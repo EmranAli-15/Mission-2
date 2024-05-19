6152d36c761a4f8ad3c681dd0b12659e1e854810
EmranAli-15 <mohammademranali15@gmail.com>
2024-05-19 23:09:43 +0600

    data posted successfully

--- dist/app/modules/student.model.js
+++ dist/app/modules/student.model.js
@@ -1,5 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.StudentModel = void 0;
 const mongoose_1 = require("mongoose");
 const userNameSchema = new mongoose_1.Schema({
     firstName: { type: String, required: true },
@@ -36,4 +37,4 @@ const studentSchema = new mongoose_1.Schema({
     profileImg: { type: String },
     isActive: ["active", "blocked"],
 });
-const Student = (0, mongoose_1.model)('Stud', studentSchema);
+exports.StudentModel = (0, mongoose_1.model)('Student', studentSchema);
--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,17 @@
 import express from 'express';
 import { Request, Response } from 'express';
 import cors from 'cors';
+import { StudentRoutes } from './app/modules/student/student.route';
 const app = express();
 
 console.log(process.cwd());
 
 app.use(express.json());
 app.use(cors());
+
+
+app.use('/api/v1/students', StudentRoutes);
+
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
