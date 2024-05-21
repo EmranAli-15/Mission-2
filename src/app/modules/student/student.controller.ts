0d5280e2fadfa67feee1a2dbe8654e852ba9ba97
EmranAli-15 <mohammademranali15@gmail.com>
2024-05-21 20:37:32 +0600

    zod perfectly worked

--- src/app/modules/student/student.controller.ts
+++ src/app/modules/student/student.controller.ts
@@ -1,21 +1,14 @@
 import { Request, Response } from "express";
 import { StudentService } from "./student.service";
-import Joi from 'joi';
-import studentValidationSchema from "./student.validation";
+import { studentValidationSchema } from "./student.validation.zod";
 
 const createStudent = async (req: Request, res: Response) => {
     try {
-        const student = req.body.student;
-        const { error } = studentValidationSchema.validate(student);
-        const result = await StudentService.createStudentIntoDB(student);
 
-        if (error) {
-            res.status(500).json({
-                success: false,
-                message: 'Something went wrong!!!',
-                error: error.details
-            })
-        }
+        
+        const student = req.body.student;
+        const zodParseData = studentValidationSchema.parse(student);
+        const result = await StudentService.createStudentIntoDB(zodParseData);
 
         res.status(200).json({
             success: true,
@@ -23,7 +16,11 @@ const createStudent = async (req: Request, res: Response) => {
             data: result,
         })
     } catch (error) {
-        // console.log(error)
+        res.status(500).json({
+            success: false,
+            message: 'Student is created successfully',
+            data: error,
+        })
     }
 };
 
