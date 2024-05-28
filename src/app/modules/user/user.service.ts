a66b597fedfd5bb5da58a8233de72d9608b23d6b
EmranAli-15 <mohammademranali15@gmail.com>
2024-05-28 18:20:18 +0600

    dynamic id generated

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,20 +1,21 @@
 import config from "../../config";
+import { AcademicSemesterModel } from "../academicSemester/academicSemester.model";
 import { Student } from "../student/student.interface";
 import { StudentModel } from "../student/student.model";
 import { TUser } from "./user.interface";
 import { User } from "./user.model";
+import { generateStudentId } from "./user.utils";
 
 const createStudentIntoDB = async (password: string, studentData: Student) => {
-
     const userData: Partial<TUser> = {};
 
     userData.password = password || config.default_pass as string;
-
     userData.role = 'student';
 
-    const generateStudentId = 
+    const admissionSemester = await AcademicSemesterModel.findById(studentData.admissionSemester);
 
-    userData.id = '202401031048'
+    userData.id = await generateStudentId(admissionSemester)
+
 
     const newUser = await User.create(userData);
 
