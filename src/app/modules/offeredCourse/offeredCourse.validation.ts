cedb7dfa05e86f0017617dc663d7c0c449eff935
EmranAli-15 <mohammademranali15@gmail.com>
2024-06-11 20:17:39 +0600

    16.10 running

--- src/app/modules/offeredCourse/offeredCourse.validation.ts
+++ src/app/modules/offeredCourse/offeredCourse.validation.ts
@@ -11,9 +11,34 @@ const createOfferedCourseValidationSchema = z.object({
         maxCapacity: z.number(),
         section: z.number(),
         days: z.array(z.enum(['Sat', 'Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri'])),
-        startTime: z.string(),
-        endTime: z.string()
-    })
+        startTime: z.string().refine(
+            (time) => {
+                const regex = /^([01]\d|2[0-3]):([0-5]\d)$/;
+                return regex.test(time);
+            },
+            {
+                message: 'Invalid time format, expected "HH:MM in 24 hours format"'
+            }
+        ),
+        endTime: z.string().refine(
+            (time) => {
+                const regex = /^([01]\d|2[0-3]):([0-5]\d)$/;
+                return regex.test(time);
+            },
+            {
+                message: 'Invalid time format, expected "HH:MM in 24 hours format"'
+            }
+        )
+    }).refine((body) => {
+        const start = new Date(`1970-01-01T${body.startTime}`);
+
+        const end = new Date(`1970-01-01T${body.endTime}`);
+
+        return end > start;
+    },
+{
+    message: 'Start time should be before end time'
+})
 });
 
 const updateOfferedCourseValidationSchema = z.object({
